a7e58ba9ad0f6b28d634061baa35cf27283c71cd
GorelovaP <komsomolskai1@mail.ru>
2022-07-15 19:59:12 +0300

    Error Page custom

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,25 +1,19 @@
 import React, {useState} from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useMatch} from 'react-router-dom'
 import s from "./Header.module.css"
 
-type ActiveType = "pre-junior" | "junior" | "junior+" | string
 
 function Header() {
-    const [active, SetActive] = useState<ActiveType>("pre-junior")
-    const tagClickHandler = (text: string) => {
-        SetActive(text.trim())
-    }
-    let ClassPreJunior = `${s.nav} ${active === "pre-junior" ? s.active : ""}`
-    let ClassJunior = `${s.nav} ${active === "junior" ? s.active : ""}`
-    let ClassJuniorPlus = `${s.nav} ${active === "junior+" ? s.active : ""}`
+    const match = useMatch('/:routeKey');
+
     return (
         <div className={s.header}>
             <div className={s.block}>
-                <NavLink onClick={(e) => tagClickHandler(e.currentTarget.text)} className={ClassPreJunior}
+                <NavLink className={`${s.nav} ${match?.params.routeKey === "pre-junior" ? s.active : undefined}`}
                          to={"/pre-junior"}> pre-junior </NavLink>
-                <NavLink onClick={(e) => tagClickHandler(e.currentTarget.text)} className={ClassJunior}
+                <NavLink className={`${s.nav} ${match?.params.routeKey === "junior" ? s.active : undefined}`}
                          to={"/junior"}> junior </NavLink>
-                <NavLink onClick={(e) => tagClickHandler(e.currentTarget.text)} className={ClassJuniorPlus}
+                <NavLink className={`${s.nav} ${match?.params.routeKey === "junior+" ? s.active : ''}`}
                          to={"/junior+"}> junior+ </NavLink>
                 <div className={s.circle}> </div>
             </div>
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import  s from "./Error404.tsx.module.css"
+import SuperButton from "../../h4/common/c2-SuperButton/SuperButton";
+import { useNavigate } from 'react-router-dom';
 
 function Error404() {
+    const navigate = useNavigate();
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.ErrorPage}>
+            <div className={s.TypeOfError}>404</div>
+            <h1>Page not found!</h1>
+            <div className={s.text}>What you are looking for does not exist or is not yet open</div>
+            <SuperButton onClick={() => navigate("/pre-junior")} >Main page</SuperButton>
         </div>
     )
 }
