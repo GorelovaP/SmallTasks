a230b8b28d188f4605daae144ecddc6ca445f6d2
GorelovaP <komsomolskai1@mail.ru>
2022-08-08 01:14:55 +0300

    Task8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,42 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type actionType = {
+    type: string;
+    payload: string
+}
+
+export const homeWorkReducer = (state: UserType[], action: actionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
+            if (action.payload === "up") {
+                let NewState = state.sort(function (a, b) {
+                    let aname = a.name.toLowerCase(),
+                        bname = b.name.toLowerCase();
+                    if (aname < bname) return -1;
+                    if (aname > bname) return 1;
+                    else return 0
+                });
+                console.log(NewState)
+                return [...NewState]
+            }
+            if (action.payload === "down") {
+                let NewState = state.sort(function (a, b) {
+                    let aname = a.name.toLowerCase(),
+                        bname = b.name.toLowerCase();
+                    if (bname < aname) return -1;
+                    if (bname > aname) return 1;
+                    else return 0
+                });
+                console.log(NewState)
+                return [...NewState]
+            }
+
             return state
         }
         case 'check': {
-            // need to fix
-            return state
+            return action.payload === "18" ? state.filter(o => o.age >= 18) : state
         }
-        default: return state
+        default:
+            return state
     }
 }
