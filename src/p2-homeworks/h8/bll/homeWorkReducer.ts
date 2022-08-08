be25eabaaddc994862d7d3a13c51258446db6654
GorelovaP <komsomolskai1@mail.ru>
2022-08-08 14:03:13 +0300

    fix immunability of reduser

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -9,32 +9,31 @@ export const homeWorkReducer = (state: UserType[], action: actionType): UserType
     switch (action.type) {
         case 'sort': {
             if (action.payload === "up") {
-                let NewState = state.sort(function (a, b) {
+                let CopyState = [...state]
+                return CopyState.sort(function (a, b) {
                     let aname = a.name.toLowerCase(),
                         bname = b.name.toLowerCase();
                     if (aname < bname) return -1;
                     if (aname > bname) return 1;
                     else return 0
-                });
-                console.log(NewState)
-                return [...NewState]
+                })
             }
             if (action.payload === "down") {
-                let NewState = state.sort(function (a, b) {
+                let CopyState = [...state]
+                return CopyState.sort(function (a, b) {
                     let aname = a.name.toLowerCase(),
                         bname = b.name.toLowerCase();
                     if (bname < aname) return -1;
                     if (bname > aname) return 1;
                     else return 0
-                });
-                console.log(NewState)
-                return [...NewState]
+                })
             }
 
             return state
         }
         case 'check': {
-            return action.payload === "18" ? state.filter(o => o.age >= 18) : state
+            let CopyState = [...state]
+            return action.payload === "18" ? CopyState.filter(o => o.age >= 18) : state
         }
         default:
             return state
