544208a77dda6de1c17f3c685dcfa6f7c9535ba0
GorelovaP <komsomolskai1@mail.ru>
2022-08-18 13:37:28 +0300

    less 9 clock

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,40 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        window.clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    let seconds, hours, minutes;
+    JSON.stringify(date.getHours()).length === 1 ? hours = "0" + date.getHours() : hours = date.getHours()
+    JSON.stringify(date.getMinutes()).length === 1 ? minutes = "0" + date.getMinutes() : minutes = date.getMinutes()
+    JSON.stringify(date.getSeconds()).length === 1 ? seconds = "0" + date.getSeconds() : seconds = date.getSeconds()
+
+    let year, month, day;
+    JSON.stringify(date.getFullYear()).length === 1 ? year = "0" + date.getFullYear() : year = date.getFullYear()
+    JSON.stringify(date.getUTCMonth()).length === 1 ? month = "0" + (date.getUTCMonth() +1) : month = date.getUTCMonth()+1
+    JSON.stringify(date.getDate()).length === 1 ? day = "0" + date.getDate() : day = date.getDate()
+
+
+    const stringTime = hours + ":" + minutes + ":" + seconds
+    const stringDate = year + "-" + month + "-" + day
 
     return (
         <div>
