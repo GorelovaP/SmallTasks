6e76d8d782fedf33d00bfe0ac0c54ab6b1ea2c20
GorelovaP <komsomolskai1@mail.ru>
2022-08-26 23:34:43 +0300

    Task 10 loader with redux

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import s from "./HW.module.css"
+
 
 function HW10() {
     // useSelector, useDispatch
-    const loading = false
+    let dispatch = useDispatch();
+    let loading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingAC(false))
         console.log('loading...')
+        setTimeout(() => dispatch(loadingAC(true)), 2000)
     };
+
 
     return (
         <div>
@@ -19,7 +26,11 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div className={s.loader}>
+                        <div className={s.innerOne}> </div>
+                        <div className={s.innerTwo}> </div>
+                        <div className={s.innerThree}> </div>
+                    </div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,29 @@
-const initState = {
-
+const initState: stateType = {
+    isLoading: false
+}
+type stateType = {
+    isLoading: boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type ActionType = loadingACType
+
+export const loadingReducer = (state: stateType = initState, action: ActionType): stateType => {
     switch (action.type) {
-        case '': {
-            return state
+        case "CHANGE-LOADING": {
+
+            return action.isLoading ? {...state, isLoading: false} : {...state, isLoading: true}
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+type loadingACType = {
+    type: "CHANGE-LOADING",
+    isLoading: boolean
+}
+
+
+export const loadingAC = (isLoading: boolean): loadingACType => {
+    return {type: "CHANGE-LOADING", isLoading: isLoading}
+}
